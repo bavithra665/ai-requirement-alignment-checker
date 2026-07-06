6b32267e7a86ed139d65b60955dbf31ba720a1e8
Bavithra <bavithra2686@gmail.com>
2026-07-06 15:21:25 +0530

    Add production frontend to CORS allowed origins

--- apps/django_api/core/settings.py
+++ apps/django_api/core/settings.py
@@ -111,7 +111,8 @@ AUTHENTICATION_BACKENDS = [
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://localhost:3001"
+    "http://localhost:3001",
+    "https://nextjs-web-cxlt.onrender.com",
 ]
 
 REST_FRAMEWORK = {
